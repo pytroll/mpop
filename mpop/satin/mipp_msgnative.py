b44f37a0256f0a9a89ee83e521138ed3e18000cc
Adam Dybbroe <adam.dybbroe@smhi.se>
2014-12-04 17:03:14 +0100

    Adding config info to the reader
    Signed-off-by: Adam Dybbroe <adam.dybbroe@smhi.se>

--- mpop/satin/mipp_msgnative.py
+++ mpop/satin/mipp_msgnative.py
@@ -24,11 +24,25 @@
 """Interface to Eumetcast level 1.5 Nativeformat. Uses the MIPP reader.
 """
 
-import logging
+import ConfigParser
+import os
 
+from datetime import datetime
+from mpop.plugin_base import Reader
+from mpop import CONFIG_PATH
+
+import logging
 LOG = logging.getLogger(__name__)
 
-from mpop.plugin_base import Reader
+try:
+    # Work around for on demand import of pyresample. pyresample might depend
+    # on scipy.spatial which memory leaks on multiple imports
+    is_pyresample_loaded = False
+    from pyresample import geometry
+    from mpop.projector import get_area_def
+    is_pyresample_loaded = True
+except ImportError:
+    LOG.warning("pyresample missing. Can only work in satellite projection")
 
 
 class NativeReader(Reader):
@@ -38,10 +52,55 @@ class NativeReader(Reader):
     def load(self, satscene, calibrate=1, filename=None, **kwargs):
         """Load the data"""
 
+        conf = ConfigParser.ConfigParser()
+        conf.read(os.path.join(CONFIG_PATH, satscene.fullname + ".cfg"))
+        options = {}
+        for option, value in conf.items(satscene.instrument_name + "-level2"):
+            options[option] = value
+
+        for section in conf.sections():
+            if(section.startswith(satscene.instrument_name) and
+               not (section == "satellite") and
+               not section[:-1].endswith("-level") and
+               not section.endswith("-granules")):
+                options[section] = conf.items(section)
+
+        for option, value in conf.items("satellite"):
+            options[option] = value
+
+        # Set the time from the filename:
+        # FIXME!
+        satscene.time_slot = datetime(2013, 11, 9, 12, 0)
+
+        # Build an area on the fly from the mipp metadata
+        proj_params = options["proj4_params"].split(" ")
+        proj_dict = {}
+        for param in proj_params:
+            key, val = param.split("=")
+            proj_dict[key] = val
+
         from mipp import native
-        image = native.MSG.NativeImage('meteosat10', filename=filename)
+        image = native.MSG.NativeImage(
+            'meteosat10', filename=filename)
 
         for channel in satscene.channels_to_load:
             print channel
             chobj = getattr(image, channel.lower())
             satscene[channel] = chobj.data
+
+            shape = chobj.data.shape
+
+            if is_pyresample_loaded:
+                # Build area_def on-the-fly
+                satscene[channel].area = geometry.AreaDefinition(
+                    satscene.satname + satscene.instrument_name +
+                    str(image.area_extent) +
+                    str(shape),
+                    "On-the-fly area",
+                    proj_dict["proj"],
+                    proj_dict,
+                    shape[1],
+                    shape[0],
+                    image.area_extent)
+            else:
+                LOG.info("Could not build area, pyresample missing...")
